2881591f45516344148fef99beb64ffc1d44992a
Steven Thomas <st01634@surrey.ac.uk>
2023-07-22 21:48:36 +0100

    added ReLU activation function

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,16 +1,22 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
 
 np.random.seed(0)
-
 #  batch of inputs per layer
 #  number of samples in batch can vary depending on problem size and dataset
 X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
+# generates data set from the package that's imported
+X, y = spiral_data(100, 3)
+
+
 # create new layer object, used to create new layers.
 # init lets us specify number of samples and number of neurons in the layer.
-#
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         # we use 0.1 to make it so that weights are between 1 and -1
@@ -20,10 +26,16 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
-# print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
+
+print(activation1.output)
+
+
